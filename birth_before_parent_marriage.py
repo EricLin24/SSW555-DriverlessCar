ecc789e0b959700130b142ab7d7f029cb8d08423
Ziang Lin <zlin17@stevens.edu>
2019-03-12 15:16:42 -0400

    add Error function into US08

--- birth_before_parent_marriage.py
+++ birth_before_parent_marriage.py
@@ -1,37 +1,46 @@
+"""US08"""
 from datetime import date
-
+import Error
 months = {'JAN': 1, 'FEB': 2, 'MAR': 3, 'APR': 4, 'MAY': 5, 'JUN': 6,
           'JUL': 7, 'AUG': 8, 'SEP': 9, 'OCT': 10, 'NOV': 11, 'DEC': 12}
 
-def birth_before_parent_marriage(birth_date, parent_marriage_date, parent_divorce_date):
+def birth_before_parent_marriage(birth_date, parent_marriage_date, parent_divorce_date, errors):
     """
     Children should be born after marriage of parents (and not more than 9 months after their divorce)
     :param member:dict
     :return: bool
     """
+    if not birth_date:
+        return errors
     err_str = f"Invalid Date birthday:{str(birth_date)} should occur after parents' marriage:{str(parent_marriage_date)} and and not more than 9 months after their divorce:{str(parent_divorce_date)}"
     birth_date = parse_date(birth_date)
     parent_marriage_date = parse_date(parent_marriage_date)
 
     if parent_marriage_date.year > birth_date.year:
-        print(err_str)
-        return False, err_str
+        us08Err = Error.Error(Error.ErrorEnum.US08)
+        us08Err.alterErrMsg(str(birth_date))
+        errors.add(us08Err)
+
     elif parent_marriage_date.year == birth_date.year and parent_marriage_date.month > birth_date.month:
-        print(err_str)
-        return False, err_str
+        us08Err = Error.Error(Error.ErrorEnum.US08)
+        us08Err.alterErrMsg(str(birth_date))
+        errors.add(us08Err)
+
     elif parent_marriage_date.year == birth_date.year and parent_marriage_date.month == birth_date.month and parent_marriage_date.day > birth_date.day:
-        print(err_str)
-        return False, err_str
+        us08Err = Error.Error(Error.ErrorEnum.US08)
+        us08Err.alterErrMsg(str(birth_date))
+        errors.add(us08Err)
+
     elif parent_divorce_date != 'NA':  #divorce
         parent_divorce_date = parse_date(parent_divorce_date)
         month = (birth_date.year - parent_divorce_date.year) * 12 + (birth_date.month - parent_divorce_date.month) + (birth_date.day > parent_divorce_date.day)
         if month > 9: #divorced beyond 9 month
-            print(err_str)
-            return False, err_str
-        else:
-            return True, 'valid'
-    else: #not divorce
-        return True, 'valid'
+            us08Err = Error.Error(Error.ErrorEnum.US08)
+            us08Err.alterErrMsg(str(birth_date))
+            errors.add(us08Err)
+
+    return errors
+
 
 def parse_date(string):
     date_list = string.split()
--- test_birth_before_parent_marriage.py
+++ test_birth_before_parent_marriage.py
@@ -1,47 +1,60 @@
 import birth_before_parent_marriage
 import unittest
+import Error
 
 class TestBirthBeforeParentMarriage(unittest.TestCase):
     #Children should be born after marriage of parents
+    def test_valid_birth_before_marriage(self):
+        birth = '20 APR 1994'
+        valid_marriage = '20 APR 1993'
+        errors = set()
+        errors = birth_before_parent_marriage.birth_before_parent_marriage(birth, valid_marriage, 'NA', errors)
+        self.assertEqual(len(errors), 0, msg='Failed to check valid birth')
+
     def test_before_marriage_year(self):
         birth = '20 APR 1994'
         invalid_marriage = '20 APR 1995'
-        valid_marriage = '20 APR 1993'
-        self.assertEqual(birth_before_parent_marriage.birth_before_parent_marriage(birth, invalid_marriage, 'NA')[0],
-                         False, msg='Should be False')
-        self.assertEqual(birth_before_parent_marriage.birth_before_parent_marriage(birth, valid_marriage, 'NA')[0],
-                         True, msg='Should be True')
+        errors = set()
+        errors = birth_before_parent_marriage.birth_before_parent_marriage(birth, invalid_marriage, 'NA', errors)
+
+        for err in errors:
+            self.assertEqual(err.errCode,
+                             Error.ErrorEnum.US08, msg='failed to generate US08 error in test_before_marriage_year')
+
 
     def test_before_marriage_month(self):
         birth = '20 APR 1994'
         invalid_marriage = '20 MAY 1994'
-        valid_marriage = '20 MAR 1994'
-        self.assertEqual(birth_before_parent_marriage.birth_before_parent_marriage(birth, invalid_marriage, 'NA')[0],
-                         False, msg='Should be False')
-        self.assertEqual(birth_before_parent_marriage.birth_before_parent_marriage(birth, valid_marriage, 'NA')[0],
-                         True, msg='Should be True')
+
+        errors = set()
+        errors = birth_before_parent_marriage.birth_before_parent_marriage(birth, invalid_marriage, 'NA', errors)
+        for err in errors:
+            self.assertEqual(err.errCode,
+                             Error.ErrorEnum.US08, msg='failed to generate US08 error in test_before_marriage_month')
+
 
     def test_before_marriage_day(self):
         birth = '20 APR 1994'
         invalid_marriage = '21 APR 1994'
-        valid_marriage = '19 APR 1994'
-        self.assertEqual(birth_before_parent_marriage.birth_before_parent_marriage(birth, invalid_marriage, 'NA')[0],
-                         False, msg='Should be False')
-        self.assertEqual(birth_before_parent_marriage.birth_before_parent_marriage(birth, valid_marriage, 'NA')[0],
-                         True, msg='Should be True')
+
+        errors = set()
+        errors = birth_before_parent_marriage.birth_before_parent_marriage(birth, invalid_marriage, 'NA', errors)
+        for err in errors:
+            self.assertEqual(err.errCode,
+                             Error.ErrorEnum.US08, msg='failed to generate US08 error in test_before_marriage_day')
 
     # not more than 9 months after divorce
     def test_after_divorce(self):
         birth = '20 APR 1994'
         valid_marriage = '19 APR 1994'
         invalid_divorce = '19 JUL 1993'
-        valid_divorce = '21 JUL 1994'
-        self.assertEqual(
-            birth_before_parent_marriage.birth_before_parent_marriage(birth, valid_marriage, invalid_divorce)[0],
-            False, msg='Should be False')
-        self.assertEqual(
-            birth_before_parent_marriage.birth_before_parent_marriage(birth, valid_marriage, valid_divorce)[0],
-            True, msg='Should be True')
+
+        errors = set()
+        errors = birth_before_parent_marriage.birth_before_parent_marriage(birth, valid_marriage, invalid_divorce, errors)
+
+        for err in errors:
+            self.assertEqual(err.errCode,
+                             Error.ErrorEnum.US08, msg='Failed to generate US08 error in test_after_divorce')
 
 if __name__ == '__main__':
     unittest.main()
