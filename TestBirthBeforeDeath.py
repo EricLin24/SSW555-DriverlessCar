458825c9853e7c9b2ca640a833437ac97f70d5c6
Jonathan <CoderInsomnia@users.noreply.github.com>
2019-03-28 15:03:28 -0400

    Update TestBirthBeforeDeath.py
    Adding console output for unit tests

--- TestBirthBeforeDeath.py
+++ TestBirthBeforeDeath.py
@@ -4,30 +4,35 @@ import BirthBeforeDeath
 class Test_Birth_Before_Parents_Death(unittest.TestCase):
 
     def test_birth_before_death_invalid_year(self):
+        print("US09: Testing birth before death of parents (invalid year)...")
         death = '1 MAR 2000'
         birth = '1 MAR 2001'
         self.assertEqual(BirthBeforeDeath.birth_before_death(death, birth), False,
                          msg='Return False: child born >= 1 year after parent death')
 
     def test_birth_before_death_invalid_month(self):
+        print("US09: Testing birth before death of parents (invalid month)...")
         death = '1 MAR 2000'
         birth = '1 APR 2000'
         self.assertEqual(BirthBeforeDeath.birth_before_death(death, birth), False,
                          msg='Return False: child born >= 1 month after parent death')
 
     def test_birth_before_death_invalid_day(self):
+        print("US09: Testing birth before death of parents (invalid day)...")
         death = '1 MAR 2000'
         birth = '2 MAR 2000'
         self.assertEqual(BirthBeforeDeath.birth_before_death(death, birth), False,
                          msg='Return False: child born >= 1 day after parent death')
 
     def test_birth_before_death_equal(self):
+        print("US09: Testing birth before death of parents (invalid)...")
         death = '1 MAR 2000'
         birth = '1 MAR 2000'
         self.assertEqual(BirthBeforeDeath.birth_before_death(death, birth), True,
                          msg='Return True: child on same day as parent death')
 
     def test_birth_before_death_valid(self):
+        print("US09: Testing birth before death of parents (valid)...")
         death = '1 MAR 2000'
         birth = '28 FEB 2000'
         self.assertEqual(BirthBeforeDeath.birth_before_death(death, birth), True,
