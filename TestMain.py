e3b8f53858668e7cecdfd2f1ec660bc68070bacb
Jonathan <CoderInsomnia@users.noreply.github.com>
2019-03-28 15:00:52 -0400

    Adding TestMain
    Added TestMain.py: executes all unit tests

---
+++ TestMain.py
@@ -0,0 +1,17 @@
+# TestMain.py - runs all availabel unit tests
+
+import unittest
+from TestDateValidation import TestDateValidation
+from TestDeathValidation import Test_Divorce_Before_Death
+from TestBirthBeforeDeath import Test_Birth_Before_Parents_Death
+from test_lessthan_150 import TestLessThan150
+from TestError import TestError
+from TestFamilyValidation import TestFamilyValidation
+from TestMarriageValidation import TestMarriageValidation
+from TestParentsTooOld import Test_Parents_Too_Old
+from TestSiblingSpacing import TestSiblingSpacing
+from test_birth_before_parent_marriage import TestBirthBeforeParentMarriage
+
+
+if __name__ == '__main__':
+    unittest.main()
