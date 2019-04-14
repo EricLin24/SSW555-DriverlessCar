07c1d3247dae69f13b8f67c1c3ec649e046c98cd
Sadie Stokes <sastokes@liberty.edu>
2019-04-14 16:25:58 -0400

    Error Message Updates

--- no_marriage_to_children.py
+++ no_marriage_to_children.py
@@ -16,14 +16,22 @@ def no_marriage_to_children(parsed_file, errors):
         if len(children_ids) <= 1:
             continue
         #children_names = [parsed_file['members']['Name'] for child_id in children_ids]
+#        print(children_ids)
         for child_id in children_ids:
             spouse_id = parsed_file['members'][child_id]['Spouse']
             spouse_id = list(spouse_id)[0] if isinstance(spouse_id, set) else spouse_id
-            #print(spouse_id)
-            if spouse_id != 'NA' and spouse_id in children_ids:
-                err_us17 = Error.Error(Error.ErrorEnum.US17)
-                married_children = ','.join([parsed_file['members'][child_id]['Name'], parsed_file['members'][spouse_id]['Name']])
-                err_us17.alterErrMsg(family_id, married_children)
-                errors.add(err_us17)
+#            print(spouse_id)
+            if spouse_id != 'NA':
+                ch1, ch2 = parsed_file['family'][spouse_id]['Spouse 1'], parsed_file['family'][spouse_id]['Spouse 2']
+                if ch1 in children_ids:
+                    err_us17 = Error.Error(Error.ErrorEnum.US17)
+                    married_children = ','.join([parsed_file['members'][child_id]['Name'], parsed_file['members'][ch1]['Name']])
+                    err_us17.alterErrMsg(family_id, married_children)
+                    errors.add(err_us17)
+                if ch2 in children_ids:
+                    err_us17 = Error.Error(Error.ErrorEnum.US17)
+                    married_children = ','.join([parsed_file['members'][child_id]['Name'], parsed_file['members'][ch2]['Name']])
+                    err_us17.alterErrMsg(family_id, married_children)
+                    errors.add(err_us17)
 
     return errors
